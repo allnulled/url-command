fa13c5decf3fe0eb610412e0de02e73288b29832
allnulled <todosanulados@gmail.com>
2025-01-25 06:06:45 +0100

    ok

--- versionate.js
+++ versionate.js
@@ -7,6 +7,9 @@ function createVersionBranch() {
     const packageJson = JSON.parse(fs.readFileSync("package.json", "utf8"));
     const version = packageJson.version;
 
+    execSync(`git add .`, { stdio: "inherit" });
+    execSync(`read -p "Escribe tu mensaje de commit (rama actual): " mensaje && git commit -m "$mensaje"`, { stdio: "inherit" });
+
     if (!version) {
       console.error("No se encontró la versión en package.json.");
       process.exit(1);
@@ -23,12 +26,13 @@ function createVersionBranch() {
     } else {
       // Crear la nueva rama basada en la actual
       execSync(`git branch ${branchName}`, { stdio: "inherit" });
-
+      
       // Subir la nueva rama al remoto
       execSync(`git push origin ${branchName}`, { stdio: "inherit" });
-
+      
       console.log(`Rama '${branchName}' creada y subida al remoto.`);
     }
+
 
     // Volver a la rama predeterminada
     execSync(`git switch main`, { stdio: "inherit" });
@@ -36,7 +40,7 @@ function createVersionBranch() {
     console.log(`Cambiado de vuelta a la rama 'main'.`);
 
     execSync(`git add .`, { stdio: "inherit" });
-    execSync(`git commit`, { stdio: "inherit" });
+    execSync(`read -p "Escribe tu mensaje de commit (rama «main»): " mensaje && git commit -m "$mensaje"`, { stdio: "inherit" });
     execSync(`git push --all`, { stdio: "inherit" });
 
   } catch (error) {
